4751a1f9e80554373d97524416828d305d7d4a93
home <self@self.com>
2024-02-28 21:28:34 +0530

    expense-terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 module "vpc" {
-  source = "./modules/vpc"
-  for_each = var.vpc
-  vpc_cidr = each.value["vpc_cidr"]
-  env      = var.env
+  source       = "./modules/vpc"
+  for_each     = var.vpc
+# vpc_cidr     = each.value["vpc_cidr"]
+  vpc_cidr     = lookup(each.value, "vpc_cidr", null)
+# subnets_cidr = each.value["subnets_cidr"]
+  subnets_cidr = lookup(each.value, "subnets_cidr", null)
+# az           = each.value["az"]
+  az           = lookup(each.value, "az" , null)
+
+  env          = var.env
   project_name = var.project_name
 }
